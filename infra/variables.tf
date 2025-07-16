b0d3793cb35b33ccc94225fc98f9d7359eefb056
Darian Huotari <darhuotari1@gmail.com>
2025-07-16 15:24:17 -0500

    [feat] - initial iac

---
+++ infra/variables.tf
@@ -0,0 +1,12 @@
+variable "site-name" {
+  type        = string
+  default     = "micro-site"
+  description = "Name of the web app. Will be used to compute dependency names"
+}
+
+variable "location" {
+  type        = string
+  default     = "East US"
+  description = ""
+}
+
