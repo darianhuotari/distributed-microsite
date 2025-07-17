bbe8881e9f0b9e484a60a490b1ce29f88ece7890
Darian Huotari <darhuotari1@gmail.com>
2025-07-17 14:37:08 -0500

    add init

--- infra/main.tf
+++ infra/main.tf
@@ -14,7 +14,7 @@ resource "azurerm_key_vault" "main" {
   location                   = azurerm_resource_group.main.location
   resource_group_name        = azurerm_resource_group.main.name
   tenant_id                  = data.azurerm_client_config.current.tenant_id
-  soft_delete_retention_days = 7
+  #soft_delete_retention_days = 7
   purge_protection_enabled   = false
   sku_name                   = "standard"
 
