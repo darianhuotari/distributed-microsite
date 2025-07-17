23b7beac3b2134fdbc7945e240db720c22feb9be
Darian Huotari <darhuotari1@gmail.com>
2025-07-17 10:51:38 -0500

    fix kv permissions

--- infra/main.tf
+++ infra/main.tf
@@ -17,6 +17,24 @@ resource "azurerm_key_vault" "main" {
   soft_delete_retention_days = 7
   purge_protection_enabled   = false
   sku_name                   = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Create",
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Set",
+      "Get",
+      "Delete",
+      "Purge",
+      "Recover"
+    ]
+  }
 }
 
 resource "azurerm_key_vault_secret" "main" {
