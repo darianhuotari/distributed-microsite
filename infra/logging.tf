114f52018f7f52385913f83ed250e9925ffb2337
Darian Huotari <darhuotari1@gmail.com>
2025-07-17 19:45:34 -0500

    add logging

--- infra/logging.tf
+++ infra/logging.tf
@@ -6,6 +6,13 @@ resource "azurerm_log_analytics_workspace" "main" {
   retention_in_days   = 30
 }
 
+resource "azurerm_application_insights" "main" {
+  name                = "${var.site-name}-appinsights"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  application_type    = "web"
+}
+
 resource "azurerm_application_insights_standard_web_test" "main" {
   name                    = "${var.site-name}-webtest"
   location                = azurerm_resource_group.main.location
