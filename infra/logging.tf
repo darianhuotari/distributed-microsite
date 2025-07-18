ce5b2b17973fdd659f43f12eeac171271ca6b187
Darian Huotari <darhuotari1@gmail.com>
2025-07-17 20:36:41 -0500

    move logic to script

--- infra/logging.tf
+++ infra/logging.tf
@@ -10,7 +10,7 @@ resource "azurerm_application_insights" "main" {
   name                = "${var.site-name}-appinsights"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
-  workspace_id        = azurerm_log_analytics_workspace.example.id
+  workspace_id        = azurerm_log_analytics_workspace.main.id
   application_type    = "other"
 }
 
